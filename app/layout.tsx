5eabcf181df2fa2f7e73644b4c7359d4e60f56bb
FafaWJr <fafawjr@gmail.com>
2026-04-08 20:48:34 +1000

    fix: serve favicon from /luna-favicon.ico to bypass CDN cache on /favicon.ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,8 +90,8 @@ export const metadata: Metadata = {
   },
 
   icons: {
-    icon: '/favicon.ico',
-    apple: '/favicon.ico',
+    icon: '/luna-favicon.ico',
+    apple: '/luna-favicon.ico',
   },
 
   other: {
